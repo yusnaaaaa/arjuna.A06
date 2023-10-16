6760f4a6d1026be6b24708836f2b3d6db37d6cba
yusnaaaaa <91377793+yusnaaaaa@users.noreply.github.com>
2023-10-16 13:13:57 +0700

    add file

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$hostname = gethostname();
+$date = date('Y-m-d H:i:s');
+$php_version = phpversion();
+$username = get_current_user();
+
+
+
+echo "Hello World!<br>";
+echo "Saya adalah: $username<br>";
+echo "Saat ini berada di: $hostname<br>";
+echo "Versi PHP yang saya gunakan: $php_version<br>";
+echo "Tanggal saat ini: $date<br>";
+?>
